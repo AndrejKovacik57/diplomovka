f7794465b2f59cc4b7a00e4237e1fb1503afea32
Andrej-pc <andrejkovacik57@gmail.com>
2025-01-23 16:17:09 +0100

    react frontend

---
+++ diplomovka/react-frontend/src/views/ExerciseCreator.tsx
@@ -0,0 +1,10 @@
+export default function ExerciseCreator() {
+    return(
+        <div>
+            <label htmlFor="name">Input exercise text:</label>
+
+            <input type="text" id="name" name="name" required />
+
+        </div>
+    )
+}
